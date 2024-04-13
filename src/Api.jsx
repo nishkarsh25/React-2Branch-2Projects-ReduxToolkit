bb778367f35467893f304cbca4a0d05e4fbfe090
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-14 02:29:34 +0530

    endpoints: An object containing endpoint definitions. Each endpoint represents a specific API operation and consists of one or more of the following:
    builder.query: Defines a query endpoint, which is used for fetching data from the server. It takes an object with the following properties:
    
    query: A function that returns the relative URL for the API endpoint.
    builder.mutation: Defines a mutation endpoint, which is used for sending data to the server and performing CRUD operations. It takes an object with the following properties:
    
    query: A function that returns an object representing the API request, including URL, method, and request body.

--- src/Api.jsx
+++ src/Api.jsx
@@ -5,7 +5,34 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
-  
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => '/posts',
+    }),
+    getUserById: builder.query({
+      query: (id) => `/posts/${id}`,
+    }),
+    addUser: builder.mutation({
+      query: (newUser) => ({
+        url: '/posts',
+        method: 'POST',
+        body: newUser,
+      }),
+    }),
+    updateUser: builder.mutation({
+      query: ({ id, updatedUser }) => ({
+        url: `/posts/${id}`,
+        method: 'PUT',
+        body: updatedUser,
+      }),
+    }),
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `/posts/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
 });
 
 
