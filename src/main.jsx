44a3e24b489f0a5a2f8e0b8c4d14711f889d4eb9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-14 03:39:14 +0530

    Mistake rectified:
    Forgot index.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './Store';
 import App from './App';
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
