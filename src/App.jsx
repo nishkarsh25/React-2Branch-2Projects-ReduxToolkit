9c1eb0f7d009bf7c3dd6958f48998c30ef9caf55
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-14 01:30:21 +0530

    App functional component:
    This component represents the main component responsible for rendering the shopping cart and product list.

--- src/App.jsx
+++ src/App.jsx
@@ -2,3 +2,6 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addToCart, updateQuantity, removeFromCart } from './CartSlice';
 
+export default function App() {
+  
+}
