5afd4e14c3dcfa482ac64475b9ba98aa81104a3b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-14 02:28:08 +0530

    reducerPath: Specifies the slice name where the generated reducer will be mounted in the Redux store.

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,3 +1,11 @@
 // api.js
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+// Create the API slice
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  
+});
+
+// Export the generated hooks for each endpoint
+export const { useGetUsersQuery, useGetUserByIdQuery, useAddUserMutation, useUpdateUserMutation, useDeleteUserMutation } = apiSlice;
