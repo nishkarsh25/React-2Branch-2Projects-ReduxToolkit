e6616804323a9b756646af4a3eb5ff6c94bd074c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-14 01:31:03 +0530

    cartItems and dispatch declaration:
    cartItems is a variable that holds the state of cart items retrieved from the Redux store using the useSelector hook.
    dispatch is a function that dispatches actions to the Redux store. It is obtained using the useDispatch hook.

--- src/App.jsx
+++ src/App.jsx
@@ -3,5 +3,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { addToCart, updateQuantity, removeFromCart } from './CartSlice';
 
 export default function App() {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const dispatch = useDispatch();
+
   
 }
